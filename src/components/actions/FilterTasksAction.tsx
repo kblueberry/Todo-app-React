19e0520bd60713e0629b19ff9a1b384230327f0c
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-03-06 17:01:44 +0200

    show tasks count in filter dropdown

--- src/components/actions/FilterTasksAction.tsx
+++ src/components/actions/FilterTasksAction.tsx
@@ -1,11 +1,21 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import Form from "react-bootstrap/Form";
 import { TasksContext } from "../../context/TasksContext";
 import { FilterCriteria } from "../../enums/Actions";
 import { APP_CONSTANTS } from "../../constants";
 
 export default function FilterTasksAction(): JSX.Element {
-  const { onTasksFiltering } = useContext(TasksContext);
+  const { onTasksFiltering, tasks } = useContext(TasksContext);
+
+  const completedTasksCount = useMemo(
+    () => tasks.filter(task => task.completed)?.length,
+    [tasks]
+  );
+
+  const currentTasksCount = useMemo(
+    () => tasks.filter(task => !task.completed)?.length,
+    [tasks]
+  );
 
   return (
     <Form.Select
@@ -16,13 +26,13 @@ export default function FilterTasksAction(): JSX.Element {
     >
       <option disabled>{APP_CONSTANTS.filterTasksLabel}</option>
       <option value={FilterCriteria.All}>
-        {APP_CONSTANTS.tasksLabels.all}
+        {APP_CONSTANTS.tasksLabels.all} ({tasks.length})
       </option>
       <option value={FilterCriteria.Completed}>
-        {APP_CONSTANTS.tasksLabels.completed}
+        {APP_CONSTANTS.tasksLabels.completed} ({completedTasksCount})
       </option>
       <option value={FilterCriteria.Current}>
-        {APP_CONSTANTS.tasksLabels.current}
+        {APP_CONSTANTS.tasksLabels.current} ({currentTasksCount})
       </option>
     </Form.Select>
   );
